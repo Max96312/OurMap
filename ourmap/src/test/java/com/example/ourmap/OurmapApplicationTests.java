b0505ae57efcea87de583a74212e283792373445
Max96312 <k0mp4ny16@gmail.com>
2023-12-05 20:15:21 +0900

    create spring

---
+++ ourmap/src/test/java/com/example/ourmap/OurmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ourmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
