cb9590fd5e826d6611470418611044ca6d54c9f5
Max96312 <k0mp4ny16@gmail.com>
2023-12-05 20:23:59 +0900

    modified /api/v1

--- ourmap/src/main/java/com/example/ourmap/controller/HelloController.java
+++ ourmap/src/main/java/com/example/ourmap/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("api/v1")
+    @GetMapping("/api/v1")
     public String hello(){
         return "test";
     }
