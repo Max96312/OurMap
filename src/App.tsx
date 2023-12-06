403d4c606df5f0470d7e31a08dc25019c640eb7b
Max96312 <k0mp4ny16@gmail.com>
2023-12-06 17:32:40 +0900

    modified front/app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Login from "./Login";
+// import Login from "./Login";
 
 function App() {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        fetch("api/v1").then((res) => {
+        fetch("/api/v1").then((res) => {
             console.log(res);
             return res.json();
         });
     }, []);
 
-    return (
-        <>
-            <Login />
-        </>
-    );
+    // return (
+    //     // <>
+    //     //     <Login />
+    //     // </>
+    // );
 }
 
 export default App;
