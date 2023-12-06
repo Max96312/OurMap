1b1214e2fc0e1dc3b7ceaedfc86a7f2510d7a64c
pparksang <pparksang1013@gmail.com>
2023-12-06 16:54:58 +0900

    F - Add froxy code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import Login from "./Login";
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <></>;
+    useEffect(() => {
+        fetch("api/v1").then((res) => {
+            console.log(res);
+            return res.json();
+        });
+    }, []);
+
+    return (
+        <>
+            <Login />
+        </>
+    );
 }
 
 export default App;
